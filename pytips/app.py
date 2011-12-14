33fd0db90776e6fbe3cc34b1edc156be62c77bde
Hank Gay <hank.gay@gmail.com>
2011-12-13 23:20:16 -0500

    Actually serve up a python tip.

--- pytips/app.py
+++ pytips/app.py
@@ -1,6 +1,10 @@
 #! /usr/bin/env python
 """The main application logic for PyTips."""
+import json
+import random
+
 from flask import Flask
+import requests
 
 
 app = Flask(__name__)
@@ -8,7 +12,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!\n'
+    my_params = {
+        "q": "#python+tip",
+        "window": "a",
+        "type": "tweet"
+    }
+    r = requests.get('http://otter.topsy.com/search.json', params=my_params)
+    response = json.loads(r.content)['response']
+    # TODO Go beyond the first page of results.
+    return random.choice(response['list'])[u'title']
 
 if __name__ == '__main__':
     app.run(debug=True)
