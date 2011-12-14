96ddbd97d1aaf7a373adb04942e2d3d17931a285
Hank Gay <hank.gay@gmail.com>
2011-12-14 07:28:21 -0500

    Choose tip from *all* results.
    Before, we were just choosing from the first page of results. Now, we perform
    a query to get the full number of results, calculate the page number and
    position on the page of a random result, and then pull that result for the
    tip.

--- pytips/app.py
+++ pytips/app.py
@@ -12,19 +12,40 @@ import requests
 
 
 app = Flask(__name__)
+QUERY = "#python+tip"
+PER_PAGE = 100
+SEARCH_COUNT_URL = 'http://otter.topsy.com/searchcount.json'
+SEARCH_URL = 'http://otter.topsy.com/search.json'
+
+
+def _get_page_and_index_for_random_tip():
+    initial_count_params = {
+        "q": QUERY
+    }
+    r = requests.get(SEARCH_COUNT_URL, params=initial_count_params)
+    response = json.loads(r.content)['response']
+    total_result_count = response['a']
+    random_index = random.randint(0, total_result_count - 1)
+    # Relies on int division truncating; this might be a Python 3 problem.
+    page_for_random_index = random_index / PER_PAGE
+    offset = PER_PAGE * page_for_random_index
+    index_on_page = random_index % PER_PAGE
+    return offset, index_on_page
 
 
 @app.route('/')
 def index():
-    my_params = {
-        "q": "#python+tip",
+    offset, index = _get_page_and_index_for_random_tip()
+    search_params = {
+        "q": QUERY,
         "window": "a",
-        "type": "tweet"
+        "offset": offset,
+        "perpage": PER_PAGE,
     }
-    r = requests.get('http://otter.topsy.com/search.json', params=my_params)
+    r = requests.get(SEARCH_URL, params=search_params)
     response = json.loads(r.content)['response']
-    # TODO Go beyond the first page of results.
-    return random.choice(response['list'])[u'title']
+    return response['list'][index]['title']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
