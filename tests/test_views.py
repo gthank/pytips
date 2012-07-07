fbe3b0e9d31fb9f2236b4f56353420b323a2f2cc
Hank Gay <hank.gay@gmail.com>
2012-07-07 16:48:46 -0400

    WIP: adding testing (had to pack up).
    --HG--
    branch : add-testing

---
+++ tests/test_views.py
@@ -0,0 +1,30 @@
+"""
+Tests for the view tier in PyTips.
+"""
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import absolute_import
+from __future__ import division
+
+
+import unittest
+from flask import Flask
+from flask.ext.testing import TestCase
+
+
+class TestViews(TestCase):
+    """Test *ALL* the Views!"""
+    def create_app(self):
+        """Creates an instance of PyTips for testing."""
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        return app
+
+    def test_index(self):
+        """Test the index page ("/")."""
+        self.assert200(self.client.get("/"))
+
+
+if __name__ == '__main__':
+    unittest.main()
+
