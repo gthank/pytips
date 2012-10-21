8ef2f7c7a2606971181ffcb968286dd321b8dcb6
Hank Gay <hank.gay@gmail.com>
2012-10-21 14:47:05 -0400

    Change default back to in-memory.
    --HG--
    branch : local-db

--- pytips/default_settings.py
+++ pytips/default_settings.py
@@ -3,4 +3,4 @@ DEBUG = True
 # If you think this is my actual secret key, I have a bridge to sell you.
 SECRET_KEY = '\x1a\x16\xd5U\xdc\x0fL\x91\x1f\x11\x08{\xa2}\xae>\xf0\x15\xd9)\xe68%!'
 # By default, just use SQLite in memory
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite://'
