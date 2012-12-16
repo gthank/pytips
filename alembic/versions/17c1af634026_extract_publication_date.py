a024e24f2c55e6d7dddbad0a1eec69e5cdca3411
Hank Gay <hank.gay@gmail.com>
2012-12-16 00:17:41 -0500

    Extract `publication_date` from `rendered_html`.
    BEFORE: We had the beautiful `publication_date` column, and we had a
    publication date buried in the `rendered_html` column, but the two
    weren't acquainted.
    
    AFTER: The `publication_date` is populated with a value extracted from
    the `rendered_html`.

---
+++ alembic/versions/17c1af634026_extract_publication_date.py
@@ -0,0 +1,44 @@
+"""Populate the `publication_date` column.
+
+Revision ID: 17c1af634026
+Revises: 3c4c29f0a791
+Create Date: 2012-12-13 21:03:03.445346
+
+"""
+# revision identifiers, used by Alembic.
+revision = '17c1af634026'
+down_revision = '3c4c29f0a791'
+
+
+import html5lib
+from dateutil.parser import parse as parse_date
+
+
+import pytips
+from pytips.models import Tip
+
+
+def _extract_publication_date(html):
+    root = html5lib.parse(html, treebuilder='lxml', namespaceHTMLElements=False)
+    publication_date_string = root.xpath("//a/@data-datetime")[0]
+    return parse_date(publication_date_string)
+
+
+def _update_tip(tip):
+    tip.publication_date = _extract_publication_date(tip.rendered_html)
+
+
+def _erase_publication_date(tip):
+    tip.publication_date = None
+
+
+def upgrade():
+    tips = Tip.query.all()
+    map(_update_tip, tips)
+    pytips.db.session.commit()
+
+
+def downgrade():
+    tips = Tip.query.all()
+    map(_erase_publication_date, tips)
+    pytips.db.session.commit()
